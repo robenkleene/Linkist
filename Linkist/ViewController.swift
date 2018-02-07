aa8afd66c33619530a2be8e714d9c0856d0718bc
Roben Kleene <robenk@gmail.com>
2018-02-06 21:53:51 -0500

    First commit

---
+++ Linkist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Linkist
+//
+//  Created by Roben Kleene on 2/6/18.
+//  Copyright © 2018 Roben Kleene. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
