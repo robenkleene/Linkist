8fdf182a9916fdc5d6fabd634980dae1aa2abda6
Roben Kleene <robenk@gmail.com>
2018-02-07 21:39:05 -0500

    Add sample command

--- Linked List/SourceEditorCommand.swift
+++ Linked List/SourceEditorCommand.swift
@@ -10,10 +10,17 @@ import Foundation
 import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
-    
+
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
         
+        // Retrieve the contents of the current source editor.
+        let lines = invocation.buffer.lines
+        // Reverse the order of the lines in a copy.
+        let updatedText = Array(lines.reversed())
+        lines.removeAllObjects()
+        lines.addObjects(from: updatedText)
+        // Signal to Xcode that the command has completed.
         completionHandler(nil)
     }
     
